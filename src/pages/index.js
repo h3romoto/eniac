b13086d37c0940c06b663df475866cc4e273c469
Tanaka <t.khondowe@gmail.com>
2022-10-03 18:19:51 -0500

    Pages outline

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Register from './Register';
+import Landing from './Landing';
+import Provision from './Provision';
+import Connect from './Provision';
+import Instance from './Instance'
+
+export default {
+  Register,
+  Landing,
+  Provision, 
+  Connect,
+  Instance
+}
